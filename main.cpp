a139a8493bfc4da7a529b5e3d3b0673dc34d14c0
aaron.hoffer <github@hoffer.org>
2022-01-18 23:23:59 -0700

    Play with z-value

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "ah_graphics.h"
 #include "ah_window.h"
+
 using namespace std;
 
 const char *vertexShaderSource();
@@ -11,6 +12,7 @@ const char *vertexShaderSource();
 const char *fragmentShaderSource();
 
 float *points();
+
 //--------------------------------------
 float span = 2.0f;
 int divisions = 50;
@@ -35,9 +37,7 @@ int main() {
     std::size_t stride = numComponents * sizeof(float);
     glVertexAttribPointer(0, numComponents, GL_FLOAT, GL_FALSE, stride, (void *) 0);
     glEnableVertexAttribArray(0);
-
     auto projection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 10.0f);
-
     auto view = glm::lookAt(
             glm::vec3(0.0f, -1.0f, 1.0f),
             glm::vec3(0.0f, 0.0f, 0.0f),
@@ -53,7 +53,6 @@ int main() {
         GLuint mvpId = glGetUniformLocation(programID, "mvp");
         auto mvp = projection * view;
         glUniformMatrix4fv(mvpId, 1, GL_FALSE, &mvp[0][0]);
-
         for (int i = 0; i < num_verts_in_a_row; i++) {
             glDrawArrays(GL_LINE_STRIP, i * num_verts_in_a_row, num_verts_in_a_row);
         }
@@ -93,9 +92,9 @@ float *points() {
             float x = step * col - 1;
             int vertex_idx = num_verts_in_a_row * row * floats_per_point + col * floats_per_point;
 //            cout << "vert_idx=" << vertex_idx << endl;
-            float z = (2.0 / (rand() % 16) + 1) - 1;
-            z=z/10.0;
-//            float z = 0.0f;
+            float z;
+            z= 0.2 / ((rand() % 4) + 1);
+//            z = 0.0f;
             pos[vertex_idx] = x;
             pos[vertex_idx + 1] = y;
             pos[vertex_idx + 2] = z;
